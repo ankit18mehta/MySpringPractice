4476072a02cee4839d70276be84bc809b0dd7adc
Ankit.Mehta <ankit.mehta@soprabanking.com>
2021-03-12 02:00:26 +0530

    Constructor & Setter DI

---
+++ SpringFrameworkPractice/src/main/java/com/am/springdemo/HappyFortuneService.java
@@ -0,0 +1,9 @@
+package com.am.springdemo;
+
+public class HappyFortuneService implements FortuneService {
+
+    public String getFortune() {
+        return "Today is your Lucky Day";
+    }
+
+}
--- SpringFrameworkPractice/src/main/java/com/am/springdemo/MyDemoApp.java
+++ SpringFrameworkPractice/src/main/java/com/am/springdemo/MyDemoApp.java
@@ -2,30 +2,24 @@ package com.am.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
+public class MyDemoApp {
 
-    public static void main(String[] args) {
+    public static void main(String args[])
 
+    {
         // load the spring configuration file
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // retrieve bean from spring container
-        Coach trackCoach = context.getBean("myCoach", Coach.class);
-        Coach baseballCoach = context.getBean("myNewCoach", Coach.class);
+
         Coach cricketCoach = context.getBean("myCricketCoach", Coach.class);
 
         // call methods on the bean
-        System.out.println(trackCoach.getDailyWorkout());
-        System.out.println(trackCoach.getDailyFortune());// HappyFortuneService Bean injected by Spring Container Using setter dependency Injection
-        System.err.println("BaseBall Coach");
-        System.out.println(baseballCoach.getDailyWorkout());
-        System.out.println(baseballCoach.getDailyFortune());// HappyFortuneService Bean injected by Spring Container Using constructor dependency Injection
-
         System.err.println("Cricket Coach");
         System.out.println(cricketCoach.getDailyWorkout());
         System.out.println(cricketCoach.getDailyFortune());// HappyFortuneService Bean injected by Spring Container Using constructor dependency Injection
         // close the context
         context.close();
-    }
 
+    }
 }
