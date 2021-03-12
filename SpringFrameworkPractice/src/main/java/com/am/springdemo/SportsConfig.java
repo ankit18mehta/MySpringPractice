ab6e05431e890efb69da1b7222df0b744a1ec947
Ankit.Mehta <ankit.mehta@soprabanking.com>
2021-03-12 05:44:25 +0530

    No Component Scan

--- SpringFrameworkPractice/src/main/java/com/am/springdemo/SportsConfig.java
+++ SpringFrameworkPractice/src/main/java/com/am/springdemo/SportsConfig.java
@@ -1,10 +1,40 @@
 package com.am.springdemo;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.am.springdemo")
+//@ComponentScan("com.am.springdemo")
 public class SportsConfig {
+
+    @Bean
+    public Coach trackCoach() {
+        TrackCoach trackCoach = new TrackCoach();
+        return trackCoach;
+    }
+
+    @Bean
+    public Coach baseballCoach() {
+        BaseballCoach baseballCoach = new BaseballCoach(happyFortuneService());
+        return baseballCoach;
+    }
+
+    @Bean
+    public Coach myCricketCoach() {
+        CricketCoach myCricketCoach = new CricketCoach();
+        return myCricketCoach;
+    }
+
+    @Bean
+    public FortuneService happyFortuneService() {
+        HappyFortuneService happyFortuneService = new HappyFortuneService();
+        return happyFortuneService;
+    }
+
+    @Bean
+    public FortuneService luckyNumberFortuneService() {
+        LuckyNumberFortuneService luckyNumberFortuneService = new LuckyNumberFortuneService();
+        return luckyNumberFortuneService;
+    }
 
 }
